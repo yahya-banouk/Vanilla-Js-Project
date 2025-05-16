dc616016ec6d8b2fc3e67e4fbd204a8594c40db9
Yahya Banouk <yahya.banouk@hotmail.com>
2025-05-17 00:18:52 +0200

    premitive and non premitive ones

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
-let test = 4;
-if test == 3 {
-  test = 5;
-  console.log(test);
-}
-else {
-  console.log(test);
-}
+// Primitive data types
+// string, number, boolean, null, undefined, symbol
+const name ="john";
+
+const age = 30;
+const isEmployed = true;
+
+const nothing = null;
+
+let undefinedVariable ;
+
+let sym = Symbol();
+
+console.log(typeof sym);
+console.log(undefinedVariable);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isEmployed);
+console.log(typeof nothing);
+
+
+// non primitive data types
+
+//literal object
+const ceties = {
+  name: "paris",
+  population: 2000000,
+};
+console.log('cities',typeof ceties);
+
+const greeting = function(){
+  console.log('hello');
+};
+console.log('greeting', typeof greeting);
+
+
+const today = new Date();
+console.log('today', typeof today);
+
+
+const arr = [1,2,3];
+
+console.log('arr',typeof arr);
