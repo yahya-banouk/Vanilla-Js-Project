1abd898d1fb431d18e4af0b0156c86d5cf849ff4
Yahya Banouk <yahya.banouk@hotmail.com>
2025-05-17 01:56:08 +0200

    operations

--- src/index.js
+++ src/index.js
@@ -1,43 +1,9 @@
-// Primitive data types
-// string, number, boolean, null, undefined, symbol
-const name ="john";
+let a = 1 +2;
+let b =  'yahya' + ' is a softawre engineer';
+let c = 1 + '4';
+let d = a / 2;
 
-const age = 30;
-const isEmployed = true;
-
-const nothing = null;
-
-let undefinedVariable ;
-
-let sym = Symbol();
-
-console.log(typeof sym);
-console.log(undefinedVariable);
-console.log(typeof name);
-console.log(typeof age);
-console.log(typeof isEmployed);
-console.log(typeof nothing);
-
-
-// non primitive data types
-
-//literal object
-const ceties = {
-  name: "paris",
-  population: 2000000,
-};
-console.log('cities',typeof ceties);
-
-const greeting = function(){
-  console.log('hello');
-};
-console.log('greeting', typeof greeting);
-
-
-const today = new Date();
-console.log('today', typeof today);
-
-
-const arr = [1,2,3];
-
-console.log('arr',typeof arr);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
