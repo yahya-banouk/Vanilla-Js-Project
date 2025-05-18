b83908d0cbd09813061168374fd9e647c3375e1c
Yahya Banouk <yahya.banouk@hotmail.com>
2025-05-18 20:23:30 +0200

    conversion

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ let b =  'yahya' + ' is a softawre engineer';
 let c = 1 + '4';
 let d = a / 2;
 
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
+let x = 1;
+x = (x++, x);
+
+;
+console.log(Number("55"));
